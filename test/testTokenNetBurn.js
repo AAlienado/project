679d812ed050c48d631fcde91e4ec93418d9a15d
Alvaro <alvaroasenjo@hotmil.com>
2018-08-21 13:45:09 +0200

    Additional testing added

--- test/testTokenNetBurn.js
+++ test/testTokenNetBurn.js
@@ -61,4 +61,176 @@ contract('BurnableToken', function(accounts) {
         await assertRevert(this.token.burn(amount, { from }));
       });
     });
+
+    describe('pause', function () {
+      const from = OWNER;
+      const amount = (4 * 10 ** 6) * 10 ** DECIMALS;
+      describe('when the sender is the token owner', function () {
+
+        describe('when the token is unpaused', function () {
+          it('pauses the token', async function (){
+            await this.token.pause({ from });
+            const paused = await this.token.paused();
+            assert.equal(paused, true);
+          });
+          it('emits a Pause event', async function () {
+            const { logs } = await this.token.pause({ from });
+            assert.equal(logs.length, 1);
+            assert.equal(logs[0].event, 'Pause');
+          });
+        });
+
+        describe('when the token is paused', function () {
+
+          beforeEach(async function () {
+              await this.token.pause();
+          });
+          it('pause reverts', async function () {
+              const paused = await this.token.paused();
+              assert.equal(paused, true);
+              await assertRevert(this.token.pause({ from }));
+          });
+          it('burn tokens reverts', async function () {
+              const paused = await this.token.paused();
+              assert.equal(paused, true);
+              await assertRevert(this.token.burn(amount, { from }));
+          });
+        });
+        describe('when the sender is not the token owner', function () {
+          const from = ANOTHER_ACCOUNT;
+
+          it('reverts', async function () {
+            await assertRevert(this.token.pause({ from }));
+          });
+        });
+      });
+    });
+
+
+    describe('unpause', function () {
+        describe('when the sender is the token owner', function () {
+          const from = OWNER;
+
+          describe('when the token is paused', function () {
+            beforeEach(async function () {
+              await this.token.pause({ from });
+            });
+
+            it('unpauses the token', async function () {
+              await this.token.unpause({ from });
+
+              const paused = await this.token.paused();
+              assert.equal(paused, false);
+            });
+
+            it('emits an Unpause event', async function () {
+              const { logs } = await this.token.unpause({ from });
+
+              assert.equal(logs.length, 1);
+              assert.equal(logs[0].event, 'Unpause');
+            });
+          });
+
+          describe('when the token is unpaused', function () {
+            it('reverts', async function () {
+              await assertRevert(this.token.unpause({ from }));
+            });
+          });
+        });
+
+        describe('when the sender is not the token owner', function () {
+          const from = ANOTHER_ACCOUNT;
+
+          it('reverts', async function () {
+            await assertRevert(this.token.unpause({ from }));
+          });
+        });
+      });
+
+
+      describe('pausable token', function () {
+        const from = OWNER;
+        describe('paused', function () {
+          it('is not paused by default', async function () {
+            const paused = await this.token.paused({ from });
+
+            assert.equal(paused, false);
+          });
+
+          it('is paused after being paused', async function () {
+            await this.token.pause({ from });
+            const paused = await this.token.paused({ from });
+
+            assert.equal(paused, true);
+          });
+
+          it('is not paused after being paused and then unpaused', async function () {
+            await this.token.pause({ from });
+            await this.token.unpause({ from });
+            const paused = await this.token.paused();
+
+            assert.equal(paused, false);
+          });
+        });
+      });
+
+      describe('transfer', function () {
+        const owner = OWNER;
+        const recipient=accounts[1]
+        it('allows to transfer when unpaused', async function () {
+          await this.token.transfer(recipient, TOTALTOKENS, { from: owner });
+
+          const senderBalance = await this.token.balanceOf(owner);
+          assert.equal(senderBalance, 0);
+
+          const recipientBalance = await this.token.balanceOf(recipient);
+          assert.equal(recipientBalance, TOTALTOKENS);
+        });
+
+        it('allows to transfer when paused and then unpaused', async function () {
+          await this.token.pause({ from: owner });
+          await this.token.unpause({ from: owner });
+
+          await this.token.transfer(recipient, TOTALTOKENS, { from: owner });
+
+          const senderBalance = await this.token.balanceOf(owner);
+          assert.equal(senderBalance, 0);
+
+          const recipientBalance = await this.token.balanceOf(recipient);
+          assert.equal(recipientBalance, TOTALTOKENS);
+        });
+
+        it('reverts when trying to transfer when paused', async function () {
+          await this.token.pause({ from: owner });
+
+          await assertRevert(this.token.transfer(recipient, TOTALTOKENS, { from: owner }));
+        });
+      });
+
+      describe('approve', function () {
+        const owner = OWNER;
+        const anotherAccount = accounts[2];
+        it('allows to approve when unpaused', async function () {
+          await this.token.approve(anotherAccount, 40, { from: owner });
+
+          const allowance = await this.token.allowance(owner, anotherAccount);
+          assert.equal(allowance, 40);
+        });
+
+        it('allows to transfer when paused and then unpaused', async function () {
+          await this.token.pause({ from: owner });
+          await this.token.unpause({ from: owner });
+
+          await this.token.approve(anotherAccount, 40, { from: owner });
+
+          const allowance = await this.token.allowance(owner, anotherAccount);
+          assert.equal(allowance, 40);
+        });
+
+        it('reverts when trying to transfer when paused', async function () {
+          await this.token.pause({ from: owner });
+
+          await assertRevert(this.token.approve(anotherAccount, 40, { from: owner }));
+        });
+      });
 });
