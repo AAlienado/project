c45933e710dc68ef7e344608af7e833d8bdcbc26
Alvaro <alvaroasenjo@hotmil.com>
2018-08-29 13:35:36 +0200

    Solhint passed and getTokensLock function added

--- contracts/TokenNETLock.sol
+++ contracts/TokenNETLock.sol
@@ -1,6 +1,7 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.4.24;
 
 import "zeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 /**
@@ -9,68 +10,77 @@ import "zeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
  * beneficiary to extract the tokens after a given release time
  */
 contract TokenTimelock {
-  using SafeERC20 for ERC20Basic;
+    using SafeERC20 for ERC20Basic;
+    using SafeMath for uint256;
 
-  // ERC20 basic token contract being held
-  ERC20Basic public token;
+    // ERC20 basic token contract being held
+    ERC20Basic public token;
 
-  // beneficiary and token number to be send after contract tokens are released
-  struct TokensLock{
-    address beneficiary;
-    uint256 numTokens;
-  }
+    // beneficiary and token number to be send after contract tokens are released
+    struct TokensLock {
+        address beneficiary;
+        uint256 numTokens;
+    }
 
-  //List with address and number of tokens lock
-  TokensLock[] private tokensLockList;
+    //List with address and number of tokens lock
+    TokensLock[] private tokensLockList;
 
-  mapping(address->uint256) indexReference;
+    mapping(address => uint256) private indexReference;
 
-  // timestamp when token release is enabled
-  uint256 public releaseTime;
+    // timestamp when token release is enabled
+    uint256 public releaseTime;
 
-  constructor(
-    ERC20Basic _token,
-    address _beneficiary,
-    uint256 _numTokens,
-    uint256 _releaseTime
-  )
-    public
-  {
-    // solium-disable-next-line security/no-block-members
-    require(_releaseTime > block.timestamp);
-    token = _token;
-    tokensLockList.push(TokensLock({beneficiary:_beneficiary,numTokens:_numTokens}));
-    indexReference[_beneficiary]=tokensLockList.lenght;
-    releaseTime = _releaseTime;
-  }
+    constructor(
+        ERC20Basic _token,
+        address _beneficiary,
+        uint256 _numTokens,
+        uint256 _releaseTime
+    )
+    public {
+      // solium-disable-next-line security/no-block-members
+        require(_releaseTime > block.timestamp);
+        token = _token;
+        tokensLockList.push(TokensLock({beneficiary:_beneficiary, numTokens:_numTokens}));
+        indexReference[_beneficiary] = tokensLockList.length;
+        releaseTime = _releaseTime;
+    }
 
   // ***Owneronly***
-  function addTokensToLock(
-    address _beneficiary,
-    uint256 _numTokens
-  )
+    function addTokensToLock(
+        address _beneficiary,
+        uint256 _numTokens
+    )
     public
-  {
-    tokensLockList.push(TokensLock({beneficiary:_beneficiary,numTokens:_numTokens}));
-  }
+    {
+        uint256 totalTokens = _numTokens;
+        uint256 id = indexReference[_beneficiary];
+        if (id != 0) {// checks if beneficiary address exists
+            id.sub(1);// tokensLockList starts with id 0
+            uint256 numTokensLocked= tokensLockList[id].numTokens;
+            totalTokens.add(numTokensLocked);
+        }
+        tokensLockList.push(TokensLock({beneficiary:_beneficiary, numTokens:totalTokens}));
+        indexReference[_beneficiary] = tokensLockList.length;
+    }
 
-  //** TODO: function to return the tokens locked by address
-  function getTokensLock(address _beneficiary) public {
+    //** TODO: function to return the tokens locked by address
+    function getTokensLock(address _beneficiary) public view returns (uint256) {
+        uint256 id = indexReference[_beneficiary] - 1;
+        return tokensLockList[id].numTokens;
+    }
 
-  }
   /**
    * @notice Transfers tokens held by timelock to beneficiary.
    */
-  function release() public {
-    // solium-disable-next-line security/no-block-members
-    require(block.timestamp >= releaseTime);
+    function release() public {
+      // solium-disable-next-line security/no-block-members
+        require(block.timestamp >= releaseTime);
 
-    uint256 amount =0;
-    for (uint i = 0; i < tokensLockList.length; i++) {
-        amount = tokensLockList[i].numTokens;
-        require(amount > 0);
-        token.safeTransfer(tokensLockList[i].beneficiary, amount);
+        uint256 amount =0;
+        for (uint i = 0; i < tokensLockList.length; i++) {
+            amount = tokensLockList[i].numTokens;
+            require(amount > 0);
+            token.safeTransfer(tokensLockList[i].beneficiary, amount);
+        }
     }
-
-  }
 }
